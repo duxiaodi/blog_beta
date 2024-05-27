ded8b075306dd188bc6113c80956225ed98f7c51
ZXT <sylvenas@outlook.com>
2024-05-27 16:14:22 +0800

    feat: 布局和小王子

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Nav from "./nav";
 
 const Header = () => {
   return (
-    <header className="sm:absolute px-5 right-0 z-[9] flex items-center h-14 flex justify-center items-center">
+    <header className="sm:absolute px-5 right-0 z-[9] flex items-center h-14 flex justify-center items-center light:bg-[#252b33]">
       <Nav />
     </header>
   );
--- components/nav.tsx
+++ components/nav.tsx
@@ -2,43 +2,46 @@ import Link from "next/link";
 import githubIcon from "../assets/images/github.svg";
 import twitterIcon from "../assets/images/twitter.svg";
 
-// TODO: 完善跳转逻辑
 function Nav() {
   return (
-    <nav className="flex items-center gap-3 md:gap-6 text-12 sm:text-13">
-      <Link as={`/`} href="/" className="cursor-pointer">
-        HOME
-      </Link>
-      <Link as={`/categories`} href="/categories" className="cursor-pointer">
-        CATEGORIES
-      </Link>
-      <Link as={`/resume`} href="/resume" className="cursor-pointer">
-        RESUME
-      </Link>
-      <Link
-        as="https://github.com/sylvenas"
-        href="https://github.com/sylvenas"
-        className="cursor-pointer"
-        target="_blank"
-      >
-        <img
-          src={githubIcon.src}
-          alt="github"
-          className="h-5 w-5 flex-shrink-0"
-        />
-      </Link>
-      <Link
-        as="https://twitter.com/TaoZhao_"
-        href="https://twitter.com/TaoZhao_"
-        className="cursor-pointer"
-        target="_blank"
-      >
-        <img
-          src={twitterIcon.src}
-          alt="twitter"
-          className="h-5 w-5 flex-shrink-0"
-        />
-      </Link>
+    <nav className="flex items-center justify-between sm:justify-start w-full gap-3 md:gap-6 text-12 sm:text-13 text-white">
+      <div className="flex items-center gap-4 md:gap-6">
+        <Link as={`/`} href="/" className="cursor-pointer">
+          HOME
+        </Link>
+        <Link as={`/categories`} href="/categories" className="cursor-pointer">
+          CATEGORIES
+        </Link>
+        <Link as={`/resume`} href="/resume" className="cursor-pointer">
+          RESUME
+        </Link>
+      </div>
+      <div className="flex items-center gap-4 md:gap-6">
+        <Link
+          as="https://github.com/sylvenas"
+          href="https://github.com/sylvenas"
+          className="cursor-pointer"
+          target="_blank"
+        >
+          <img
+            src={githubIcon.src}
+            alt="github"
+            className="h-5 w-5 flex-shrink-0"
+          />
+        </Link>
+        <Link
+          as="https://twitter.com/TaoZhao_"
+          href="https://twitter.com/TaoZhao_"
+          className="cursor-pointer"
+          target="_blank"
+        >
+          <img
+            src={twitterIcon.src}
+            alt="twitter"
+            className="h-5 w-5 flex-shrink-0"
+          />
+        </Link>
+      </div>
     </nav>
   );
 }
